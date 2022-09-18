2e34d29ed001cee06e8e6bd2e6cf9c793157ed75
amolmane7471 <maneamol3233@gmail.com>
2022-09-19 01:13:11 +0530

    [Amol]  : added cab invoice generator

---
+++ src/main/java/com/bridgelabz/CabInvoice/CabInvoiceGenerator.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.CabInvoice;
+
+public class CabInvoiceGenerator {
+    private static final int MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+
+    public static double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+    }
+    public static void main(String[] args) {
+        System.out.println("****Welcome to CabInvoiceGenerator Application****");
+        System.out.println(calculateFare(2.0, 5));
+    }
+}
